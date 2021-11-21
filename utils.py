e0a9561884d7933b6b72717a71a180257eeeec7e
bipoza <bipoza@gmail.com>
2021-11-21 20:46:31 +0100

    karpeta ezabatzeko funtzioa sinplifikatuta

--- utils.py
+++ utils.py
@@ -49,14 +49,6 @@ def create_folder(path):
 def remove_folder(path):
     import os, shutil
     if os.path.exists(path):
-      for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        shutil.rmtree(path)
     else:
         print("The folder does not exist")
